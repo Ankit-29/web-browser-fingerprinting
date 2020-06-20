3c22d937f42f5b567e2b67fbab49ec22df2d82a9
Ankit-29 <iamankitkumar29@gmail.com>
2020-06-20 22:41:24 +0530

    update: Added Ip along with browser deails

--- js/web-browser-fingerprint.js
+++ js/web-browser-fingerprint.js
@@ -1,17 +1,33 @@
 getBrowserFingerPrint = () => {
-    return btoa(JSON.stringify(getBrowserDetails()));
+    getBrowserDetails().then(res => {
+        console.log(btoa(JSON.stringify(res)));
+        document.querySelector('span').innerHTML = btoa(JSON.stringify(res));
+        // return btoa(JSON.stringify(res));
+    });
 }
 
 getBrowserDetails = () => {
-    const detailsObject = {
-        userAgent: navigator.userAgent,
-        hardwareConcurrency: navigator.hardwareConcurrency,
-        height: window.screen.height,
-        width: window.screen.width,
-        maxTouchPoints: navigator.maxTouchPoints,
-        colorDepth: window.screen.colorDepth,
-        isCookieEnabled: navigator.cookieEnabled
-    }
+    return new Promise((res, rej) => {
+        const detailsObject = {
+            userAgent: navigator.userAgent,
+            hardwareConcurrency: navigator.hardwareConcurrency,
+            height: window.screen.height,
+            width: window.screen.width,
+            maxTouchPoints: navigator.maxTouchPoints,
+            colorDepth: window.screen.colorDepth,
+            isCookieEnabled: navigator.cookieEnabled,
+            ip: null
+        }
 
-    return detailsObject;
+        fetch('https://api.ipify.org/?format=json')
+            .then(res => res.json())
+            .then(ipData => {
+                detailsObject.ip = ipData.ip;
+                res(detailsObject);
+            }).catch(err => {
+                console.error(err);
+                res(detailsObject);
+            });
+    });
+
 }
